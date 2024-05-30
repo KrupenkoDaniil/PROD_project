d3a1320eb437269b43fdeb7884ddcb73e887346b
Krupenko_Daniil <daniilkru24@gmail.com>
2024-05-30 20:20:10 +0300

    test

--- tests/integration/utils/resume-builder.po.js
+++
@@ -1,8 +0,0 @@
-const {expect} = require('@playwright/test');
-
-export async function generateResume(page) {
-  const generateResumeButton = await page.getByTestId('generate-resume');
-  await expect(generateResumeButton).toBeVisible();
-  await expect(generateResumeButton).toBeEnabled();
-  await generateResumeButton.click();
-}
